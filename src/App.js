95acf1965d9eabb8d71248b09e7ee537c5f45dad
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-07-14 03:57:35 -0700

    Managed State for Adding and Deleting Books using Redux.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
+import { useSelector } from 'react-redux/es/exports';
 import BookContainer from './components/BookContainer';
 
-const books = [
-  {
-    id: 2,
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    category: 'Action',
+// const books = [
+//   {
+//     id: 2,
+//     title: 'The Hunger Games',
+//     author: 'Suzanne Collins',
+//     category: 'Action',
 
-  },
-];
+//   },
+// ];
 
 function App() {
+  const books = useSelector((state) => state.books);
   return (
     <div className="App">
       <BookContainer books={books} />
--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux/es/exports';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 import './AddBook.css';
 
-const AddBook = () => (
-  <div className="inputContainer">
-    <h2 className="FormText">ADD NEW BOOK</h2>
-    <form action="#">
-      <input type="text" name="title" className="inputTitle" placeholder="Book Title" id="input0" />
-      <input type="text" name="author" className="inputAuthor" placeholder="Book Author" id="input1" />
-      <button type="submit" className="FormBtn">Add Book</button>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title !== '' && author !== '') {
+      dispatch(
+        addBook({
+          id: uuidv4(),
+          cat: 'Action',
+          title,
+          author,
+          comments: [],
+          progress: 64,
+          chapters: 20,
+          currentChapter: { chapter: 17, chapterTitle: 'Hunger is no game' },
+          category: 'Add_book',
+        }),
+      );
+    }
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <div className="inputContainer">
+      <h2 className="FormText">ADD NEW BOOK</h2>
+      <form
+        action="#"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} className="inputTitle" placeholder="Book Title" id="input0" />
+        <input type="text" name="author" value={author} className="inputAuthor" onChange={(e) => setAuthor(e.target.value)} placeholder="Book Author" id="input1" />
+        <button onClick={handleSubmit} type="submit" className="FormBtn">
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
